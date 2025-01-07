8414deb908eceda1a85dc88011d8e4c82bd65439
Raisa <hello@raisa.com.np>
2025-01-08 00:06:57 +0545

    Routes links added

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function LoginPage() {
     const [email, setEmail] = useState('');
